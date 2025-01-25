58152a4ffa58352b570409660304e65b6b2cd859
Vjacheslav Trushkin <cyberalien@gmail.com>
2025-01-25 21:09:22 +0200

    chore: working version

--- packages/plugin/src/plugin.ts
+++ packages/plugin/src/plugin.ts
@@ -3,47 +3,45 @@ import { getDynamicCSSRules } from './plugins/dynamic.js';
 import type { DynamicIconifyPluginOptions } from './helpers/options.js';
 
 const exportedPlugin = plugin.withOptions((params: unknown) => {
+	// Clean up options
+	const options: DynamicIconifyPluginOptions = {};
+	Object.entries(params ?? {}).forEach(([key, value]) => {
+		switch (key) {
+			case 'prefix':
+				if (typeof value === 'string') {
+					options.prefix = value;
+				}
+				break;
+
+			case 'overrideOnly':
+			case 'override-only':
+			case 'overrideonly':
+				if (value === true) {
+					options.overrideOnly = true;
+				}
+				break;
+
+			case 'scale':
+				if (typeof value === 'number') {
+					options.scale = value;
+				}
+				break;
+		}
+	});
+
+	const prefix = options.prefix || 'icon';
 	return ({ matchComponents }) => {
-		// Clean up options
-		const options: DynamicIconifyPluginOptions = {};
-		Object.entries(params ?? {}).forEach(([key, value]) => {
-			switch (key) {
-				case 'prefix':
-					if (typeof value === 'string') {
-						options.prefix = value;
-					}
-					break;
-
-				case 'overrideOnly':
-				case 'override-only':
-				case 'overrideonly':
-					if (value === true) {
-						options.overrideOnly = true;
-					}
-					break;
-
-				case 'scale':
-					if (typeof value === 'number') {
-						options.scale = value;
-					}
-					break;
-			}
+		matchComponents({
+			[prefix]: (icon: string) => {
+				try {
+					return getDynamicCSSRules(icon);
+				} catch (err) {
+					// Log error, but do not throw it
+					console.warn((err as Error).message);
+					return {};
+				}
+			},
 		});
-
-		const prefix = options.prefix || 'icon';
-		return ({ matchComponents }) => {
-			matchComponents({
-				[prefix]: (icon: string) => {
-					try {
-						return getDynamicCSSRules(icon);
-					} catch (err) {
-						// Log error, but do not throw it
-						console.warn((err as Error).message);
-						return {};
-					}
-				},
-			});
-		};
 	};
 });
 
