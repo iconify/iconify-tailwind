4703dc0915b6957874716a9aa6d30e9bd881fd96
Vjacheslav Trushkin <cyberalien@gmail.com>
2025-10-30 08:49:26 +0200

    chore: fix dependencies and plugin building

--- packages/plugin/src/plugin.ts
+++ packages/plugin/src/plugin.ts
@@ -36,120 +36,124 @@ function getFloatValue(value: unknown, defaultValue: number): number {
 	return defaultValue;
 }
 
-const exportedPlugin = plugin.withOptions((params: unknown) => {
-	// Clean up options
-	const dynamicOptions: DynamicIconifyPluginOptions = {};
-	const preparsedOptions: PreparsedIconifyPluginOptions = {};
-	// console.log('Params:', JSON.stringify(params, null, 2));
-	Object.entries(params ?? {}).forEach(([key, value]) => {
-		switch (key) {
-			// Options for dynamic plugin
-			case 'prefix':
-				if (value === false) {
-					// Empty prefix: disables plugin
-					dynamicOptions.prefix = '';
-				}
-				if (typeof value === 'string') {
-					dynamicOptions.prefix = value;
-				}
-				return;
-
-			case 'overrideOnly':
-			case 'override-only':
-			case 'overrideonly':
-				dynamicOptions.overrideOnly = getBooleanValue(
-					value,
-					dynamicOptions.overrideOnly ?? false
-				);
-				return;
-
-			// Options for preparsed plugin
-			case 'prefixes': {
-				// prefixes: foo;
-				if (typeof value === 'string') {
-					preparsedOptions.prefixes = [value];
-					return;
-				}
-				// prefixes: foo, bar;
-				if (Array.isArray(value)) {
-					preparsedOptions.prefixes = value;
-					return;
-				}
-				return;
-			}
-
-			case 'iconSelector':
-			case 'icon-selector':
-			case 'iconselector':
-				if (typeof value === 'string') {
-					preparsedOptions.iconSelector = value;
-				}
-				return;
-
-			case 'maskSelector':
-			case 'mask-selector':
-			case 'maskselector':
-				if (typeof value === 'string') {
-					preparsedOptions.maskSelector = value;
-				}
-				return;
-
-			case 'backgroundSelector':
-			case 'background-selector':
-			case 'backgroundselector':
-				if (typeof value === 'string') {
-					preparsedOptions.backgroundSelector = value;
-				}
-				return;
-
-			case 'varName':
-			case 'var-name':
-			case 'varname':
-				if (typeof value === 'string') {
-					preparsedOptions.varName = value;
-				}
-				return;
-
-			case 'square':
-				preparsedOptions.square = getBooleanValue(
-					value,
-					preparsedOptions.square ?? true
-				);
-				return;
-
-			// Common options
-			case 'scale': {
-				const scale = getFloatValue(value, dynamicOptions.scale ?? 1);
-				dynamicOptions.scale = scale;
-				preparsedOptions.scale = scale;
-				return;
-			}
-		}
-	});
-
-	return ({ matchComponents, addComponents, addUtilities }) => {
-		// Dynamic plugin
-		const prefix = dynamicOptions.prefix ?? 'icon';
-		if (prefix) {
-			matchComponents({
-				[prefix]: (icon: string) => {
-					try {
-						return getDynamicCSSRules(icon, dynamicOptions);
-					} catch (err) {
-						// Log error, but do not throw it
-						console.warn((err as Error).message);
-						return {};
+const exportedPlugin: ReturnType<typeof plugin.withOptions> =
+	plugin.withOptions((params: unknown) => {
+		// Clean up options
+		const dynamicOptions: DynamicIconifyPluginOptions = {};
+		const preparsedOptions: PreparsedIconifyPluginOptions = {};
+		// console.log('Params:', JSON.stringify(params, null, 2));
+		Object.entries(params ?? {}).forEach(([key, value]) => {
+			switch (key) {
+				// Options for dynamic plugin
+				case 'prefix':
+					if (value === false) {
+						// Empty prefix: disables plugin
+						dynamicOptions.prefix = '';
 					}
-				},
-			});
-		}
+					if (typeof value === 'string') {
+						dynamicOptions.prefix = value;
+					}
+					return;
 
-		// Preparsed options
-		if (preparsedOptions.prefixes) {
-			addComponents(getCSSComponentsForPlugin(preparsedOptions));
-			addUtilities(getCSSRulesForPlugin(preparsedOptions));
-		}
-	};
-});
+				case 'overrideOnly':
+				case 'override-only':
+				case 'overrideonly':
+					dynamicOptions.overrideOnly = getBooleanValue(
+						value,
+						dynamicOptions.overrideOnly ?? false
+					);
+					return;
+
+				// Options for preparsed plugin
+				case 'prefixes': {
+					// prefixes: foo;
+					if (typeof value === 'string') {
+						preparsedOptions.prefixes = [value];
+						return;
+					}
+					// prefixes: foo, bar;
+					if (Array.isArray(value)) {
+						preparsedOptions.prefixes = value;
+						return;
+					}
+					return;
+				}
+
+				case 'iconSelector':
+				case 'icon-selector':
+				case 'iconselector':
+					if (typeof value === 'string') {
+						preparsedOptions.iconSelector = value;
+					}
+					return;
+
+				case 'maskSelector':
+				case 'mask-selector':
+				case 'maskselector':
+					if (typeof value === 'string') {
+						preparsedOptions.maskSelector = value;
+					}
+					return;
+
+				case 'backgroundSelector':
+				case 'background-selector':
+				case 'backgroundselector':
+					if (typeof value === 'string') {
+						preparsedOptions.backgroundSelector = value;
+					}
+					return;
+
+				case 'varName':
+				case 'var-name':
+				case 'varname':
+					if (typeof value === 'string') {
+						preparsedOptions.varName = value;
+					}
+					return;
+
+				case 'square':
+					preparsedOptions.square = getBooleanValue(
+						value,
+						preparsedOptions.square ?? true
+					);
+					return;
+
+				// Common options
+				case 'scale': {
+					const scale = getFloatValue(
+						value,
+						dynamicOptions.scale ?? 1
+					);
+					dynamicOptions.scale = scale;
+					preparsedOptions.scale = scale;
+					return;
+				}
+			}
+		});
+
+		return ({ matchComponents, addComponents, addUtilities }) => {
+			// Dynamic plugin
+			const prefix = dynamicOptions.prefix ?? 'icon';
+			if (prefix) {
+				matchComponents({
+					[prefix]: (icon: string) => {
+						try {
+							return getDynamicCSSRules(icon, dynamicOptions);
+						} catch (err) {
+							// Log error, but do not throw it
+							console.warn((err as Error).message);
+							return {};
+						}
+					},
+				});
+			}
+
+			// Preparsed options
+			if (preparsedOptions.prefixes) {
+				addComponents(getCSSComponentsForPlugin(preparsedOptions));
+				addUtilities(getCSSRulesForPlugin(preparsedOptions));
+			}
+		};
+	});
 
 export default exportedPlugin;
