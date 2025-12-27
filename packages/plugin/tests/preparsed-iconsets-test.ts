2ee9fdefc05570759c31dadba6cef3aa348ebcb5
Vladislav Gapurov <gapurov@gmail.com>
2025-12-27 18:07:22 +0100

    test(plugin): cover mixed custom and standard prefixes

--- packages/plugin/tests/preparsed-iconsets-test.ts
+++ packages/plugin/tests/preparsed-iconsets-test.ts
@@ -1,18 +1,18 @@
 import { getCSSRulesForPlugin } from '../lib/plugins/preparsed.js';
 
+const customIconSet = {
+	prefix: 'custom-icon-prefix',
+	icons: {
+		home: {
+			body: '<path d="M0 0h24v24H0z" fill="currentColor"/>',
+			width: 24,
+			height: 24,
+		},
+	},
+};
+
 describe('Preparsed icon sets', () => {
 	it('Loads icon set from iconSets map for string prefixes', () => {
-		const customIconSet = {
-			prefix: 'custom-icon-prefix',
-			icons: {
-				home: {
-					body: '<path d="M0 0h24v24H0z" fill="currentColor"/>',
-					width: 24,
-					height: 24,
-				},
-			},
-		};
-
 		const rules = getCSSRulesForPlugin({
 			prefixes: ['custom-icon-prefix'],
 			iconSets: {
@@ -21,5 +21,17 @@ describe('Preparsed icon sets', () => {
 		});
 
 		expect(rules['.custom-icon-prefix--home']).toBeDefined();
+	});
+
+	it('Loads custom and standard icon sets together', () => {
+		const rules = getCSSRulesForPlugin({
+			prefixes: ['custom-icon-prefix', 'mdi-light'],
+			iconSets: {
+				'custom-icon-prefix': customIconSet,
+			},
+		});
+
+		expect(rules['.custom-icon-prefix--home']).toBeDefined();
+		expect(rules['.mdi-light--home']).toBeDefined();
 	});
 });
